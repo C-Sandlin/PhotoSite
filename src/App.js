3cb8ab0d2baf938cb106357f508b148e726f9822
Colin Sandlin <csandlin010@gmail.com>
2019-10-22 19:53:37 -0500

    setup Routing in app home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import TopNav from './components/nav/TopNav.js'
 import "./styles/main.scss";
 import AboutMe from "./components/aboutme/AboutMe"
+import LandingPage from './components/landing/LandingPage.js';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 function App() {
+
+  const state = {
+    status: "placeholder",
+  };
+
   return (
     <div className="App">
-      <TopNav />
-      <h1>App</h1>
+      <Route exact path="/home">
+        <TopNav />
+        <LandingPage />
+      </Route>
+      <Route exact path="/journeys">
+        <TopNav />
+        {/* replace with actual */}
+        {/* <LandingPage /> */}
+      </Route>
     </div>
   );
 }
---
+++ src/components/landing/LandingPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import mainLogo from "../../assets/cs-main-logo.svg";
+// import { IoIosCloseCircleOutline, IoIosArrowDown } from "react-icons/io";
+// import { IconContext } from "react-icons";
+
+function LandingPage() {
+    return (
+        <>
+            <section className="landing-section">
+                <div className="outer-landing">
+
+                </div>
+                <div className="inner-landing">
+
+                </div>
+            </section>
+        </>
+    )
+};
+
+export default LandingPage;
