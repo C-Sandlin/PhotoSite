a8726786cf9126c791554dd60c4aa5fc16164992
Colin Sandlin <csandlin010@gmail.com>
2019-11-07 19:12:29 -0600

    landing page setup

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -2,18 +2,17 @@ import React from 'react';
 import mainLogo from "../../assets/cs-main-logo.svg";
 // import { IoIosCloseCircleOutline, IoIosArrowDown } from "react-icons/io";
 // import { IconContext } from "react-icons";
+import "./landing.scss";
 
 function LandingPage() {
     return (
         <>
-            <section className="landing-section">
-                <div className="outer-landing">
-
-                </div>
+            <div className="outer-landing">
                 <div className="inner-landing">
-
+                    <div class="landing-project" id="newZealand-proj">
+                    </div>
                 </div>
-            </section>
+            </div>
         </>
     )
 };
