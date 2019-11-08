1965c7fcc95fdffbd8698ec5d095827522a741e5
Colin Sandlin <csandlin010@gmail.com>
2019-11-07 21:07:32 -0600

    mouse wheel to switch projects now works rudimentarily

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -4,17 +4,51 @@ import mainLogo from "../../assets/cs-main-logo.svg";
 // import { IconContext } from "react-icons";
 import "./landing.scss";
 
-function LandingPage() {
-    return (
-        <>
-            <div className="outer-landing">
-                <div className="inner-landing">
-                    <div class="landing-project" id="newZealand-proj">
+
+export default class LandingPage extends React.Component {
+
+    componentDidMount = () => {
+        document.getElementById('outer-landing').addEventListener('wheel', this.handleScroll, true);
+    }
+
+    componentWillUnmount = () => {
+        window.getElementById('outer-landing').removeEventListener('wheel', this.handleScroll);
+    }
+
+    handleScroll = (event) => {
+        if (event.deltaY > 120) {
+            let currentProj = event.target;
+            currentProj.classList.remove('current');
+            currentProj.classList.add('not-current');
+
+            let nextProj = currentProj.nextSibling;
+            nextProj.classList.remove('not-current');
+            nextProj.classList.add('current');
+        }
+
+        if (event.deltaY < -120) {
+            let currentProj = event.target;
+            currentProj.classList.remove('current');
+            currentProj.classList.add('not-current');
+
+            let nextProj = currentProj.previousSibling;
+            nextProj.classList.remove('not-current');
+            nextProj.classList.add('current');
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <div className="outer-landing" id="outer-landing">
+                    <div className="inner-landing" >
+                        <div className="landing-project current" id="newZealand-proj">
+                        </div>
+                        <div className="landing-project  not-current" id="italy-proj">
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
-};
-
-export default LandingPage;
+            </>
+        )
+    };
+}
