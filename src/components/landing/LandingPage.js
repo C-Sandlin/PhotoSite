28510ac84f5dae9471f396b0baec20b580eae748
Colin Sandlin <csandlin010@gmail.com>
2019-11-09 10:57:50 -0600

    fixed scrolling through projects

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -17,16 +17,17 @@ export default class LandingPage extends React.Component {
 
     handleScroll = (event) => {
         let currentProj = event.target;
+        let nextProj = currentProj.nextSibling;
+        let prevProj = currentProj.previousSibling;
 
         // check intertia of downward scroll
         if (event.deltaY > 120) {
-            let nextProj = currentProj.nextSibling;
 
             if (nextProj !== null) {
                 currentProj.classList.remove('current');
-                currentProj.classList.add('not-current');
+                currentProj.classList.add('above');
 
-                nextProj.classList.remove('not-current');
+                nextProj.classList.remove('below');
                 nextProj.classList.add('current');
             } else {
                 return;
@@ -35,13 +36,12 @@ export default class LandingPage extends React.Component {
 
         // check intertia of upward scroll
         if (event.deltaY < -120) {
-            let prevProj = currentProj.previousSibling;
 
             if (prevProj !== null) {
                 currentProj.classList.remove('current');
-                currentProj.classList.add('not-current');
+                currentProj.classList.add('below');
 
-                prevProj.classList.remove('not-current');
+                prevProj.classList.remove('above');
                 prevProj.classList.add('current');
             }
         }
@@ -52,9 +52,13 @@ export default class LandingPage extends React.Component {
             <>
                 <div className="outer-landing" id="outer-landing">
                     <div className="inner-landing" >
-                        <div className="landing-project current" id="newZealand-proj">
+                        <div className="landing-project above" id="newZealand-proj">
                         </div>
-                        <div className="landing-project  not-current" id="italy-proj">
+                        <div className="landing-project  current" id="sanDiego-proj">
+                        </div>
+                        <div className="landing-project  below" id="arizona-proj">
+                        </div>
+                        <div className="landing-project  below" id="italy-proj">
                         </div>
                     </div>
                 </div>
